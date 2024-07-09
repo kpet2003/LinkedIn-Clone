f48673731f3b00a25c156d8074a3a10a357a30ff
almantz <lsmantzari@gmail.com>
2024-07-09 14:13:54 +0300

    created java spring boot project

---
+++ tedi/src/main/java/com/tediproject/tedi/TediApplication.java
@@ -0,0 +1,13 @@
+package com.tediproject.tedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TediApplication.class, args);
+	}
+
+}
---
+++ tedi/src/test/java/com/tediproject/tedi/TediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tediproject.tedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
