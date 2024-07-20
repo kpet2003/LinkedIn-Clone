7276c17de75a025a797a09a1f5886e3e42075a7c
kpet2003 <konpet2003@gmail.com>
2024-07-20 19:02:58 +0300

    started database

---
+++ tedi/src/main/java/com/tediproject/tedi/controllers/UserControllers.java
@@ -0,0 +1,9 @@
+package com.tediproject.tedi.controllers;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserControllers {
+    
+
+
+}
---
+++ tedi/src/main/java/com/tediproject/tedi/model/User.java
@@ -0,0 +1,34 @@
+package com.tediproject.tedi.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+    
+    // primary key of User table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    protected long id;
+    
+    @Column
+    protected String first_name;
+    @Column
+    protected String last_name;
+    @Column
+    protected String email;
+    @Column
+    protected String password;
+    @Column
+    protected long phone_number;
+    @Column
+    protected String image_path;
+    @Column
+    protected String resume_path;
+
+    public User() {}
+
+}
