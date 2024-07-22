16884f51a07807ae2b57aeb67daaf0076b136c98
kpet2003 <konpet2003@gmail.com>
2024-07-22 19:08:26 +0300

    next :(

---
+++ my-app/src/app/app.js
@@ -0,0 +1,15 @@
+function welcome_page() {
+    return(
+        <div>
+            <button>
+                sign up
+            </button>
+
+            <button>
+                login
+            </button>
+        </div>
+    );
+}
+
+export default welcome_page;
