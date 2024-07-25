01f54a68c84d13b5ec570cc5f8bbe694022c09fc
almantz <lsmantzari@gmail.com>
2024-07-25 19:29:54 +0300

    bla

---
+++ tedi/src/main/java/com/tediproject/tedi/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.tediproject.tedi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String redirect() {
+        // Forward to index.html
+        return "forward:/index.html";
+    }
+}
