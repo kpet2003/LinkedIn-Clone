c1cfac103a4b8eb5ea9fa58ab8d245ad97347645
kpet2003 <konpet2003@gmail.com>
2024-09-19 20:51:41 +0300

    install

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # setup mysql database
-mysql -u root -ppassword -h localhost  -p < /home/kon/tedi/tedi/database.sql 
+mysql -u root -ppassword -h localhost  -p < tedi/database.sql 
 
 # setup react for the frontend
-cd ../frontend
+cd  frontend
 npm update
 npm start &
 
+sleep 10
 cd ../tedi
 mvn spring-boot:run 
